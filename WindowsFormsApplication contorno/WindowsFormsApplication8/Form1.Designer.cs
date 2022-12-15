cc920bc05cd64df435a90d9174589253d5d97d12
JcarloS-HA <84425634+JcarloS-HA@users.noreply.github.com>
2022-12-15 00:37:17 -0400

    Add files via upload

---
+++ WindowsFormsApplication contorno/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(44)))), ((int)(((byte)(79)))));
+            this.button7.Font = new System.Drawing.Font("Adobe Gothic Std B", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(219)))), ((int)(((byte)(229)))));
+            this.button7.Location = new System.Drawing.Point(10, 71);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(144, 31);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "BORDES";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(44)))), ((int)(((byte)(79)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Adobe Gothic Std B", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(219)))), ((int)(((byte)(229)))));
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ABRIR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(245)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(165, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(575, 525);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(44)))), ((int)(((byte)(79)))));
+            this.button2.Font = new System.Drawing.Font("Adobe Gothic Std B", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(219)))), ((int)(((byte)(229)))));
+            this.button2.Location = new System.Drawing.Point(10, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 31);
+            this.button2.TabIndex = 42;
+            this.button2.Text = "LIMPIAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(228)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(764, 553);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PREGUNTA 2 RECUPERATORIO - Juan Carlos Huaranca A";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication contorno/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Bitmap bmpBK;
+        //int x, y;
+        int []xx=new int[10];
+        int []yy=new int[10];
+
+        int[] rr = new int[4];
+        int[] gg = new int[4];
+        int[] bb = new int[4];
+
+
+        //BufferedImage Nueva_Imagen,imagen;
+         Bitmap Nueva_Imagen;
+	    int i,j,r,g,b,w,h;
+	    float sum = 0;
+	    int[,] rojo,verde,azul,HorizontalR,VerticalR,HorizontalG,VerticalG,HorizontalB,VerticalB;
+	    //int a[];    
+	    Color colorAuxiliar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Todos|*.*|Archivos JPEG|*.jpg|Archivos GIF|*.gif";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            bmp = new Bitmap(openFileDialog1.FileName);
+            bmpBK = bmp;
+            pictureBox1.Image = bmp;
+            inicio();
+        }
+        void inicio(){
+        	Nueva_Imagen=new Bitmap(bmp.Width, bmp.Height) ;
+		    h=bmp.Height;
+		    w=bmp.Width;
+		    rojo=new int[w,h];
+		    verde=new int[w,h];
+		    azul=new int[w,h];
+		    HorizontalR=new int[w,h];
+		    HorizontalG=new int[w,h];
+		    HorizontalB=new int[w,h];
+		    VerticalR=new int[w,h];
+		    VerticalG=new int[w,h];
+		    VerticalB=new int[w,h];
+            int asa=w*h;
+		    int []a=new int [asa];
+		    for( i=0;i<bmp.Width;i++){
+			    for(j=0;j<bmp.Height;j++){
+				    colorAuxiliar=new Color();
+                    colorAuxiliar = bmp.GetPixel(i, j);
+				    r = colorAuxiliar.R;
+				    g= colorAuxiliar.G;
+				    b = colorAuxiliar.B;
+				    rojo[i,j]=r; 
+				    verde[i,j]=g; 
+				    azul[i,j]=b; 
+			    }
+		    }
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+
+        void OperacionVertical() {
+            for (i = 0; i < bmp.Width; i++)
+            {
+                for (j = 0; j < bmp.Height - 1; j++)
+                {
+                    VerticalR[i,j] = Math.Abs((rojo[i,j + 1] - rojo[i,j]));
+                    VerticalG[i,j] = Math.Abs((verde[i,j + 1] - verde[i,j]));
+                    VerticalB[i,j] = Math.Abs((azul[i,j + 1] - azul[i,j]));
+                    Nueva_Imagen.SetPixel(i, j, Color.FromArgb(VerticalR[i,j], VerticalG[i,j], VerticalB[i,j]));
+                    //Nueva_Imagen.SetPixel(i, j, Color.FromArgb(VerticalR[i,j], VerticalG[i,j], VerticalB[i,j]).getPixel());
+                }
+            }
+        }
+        void OperacionHorizontal(){
+            for (i = 0; i < bmp.Width; i++)
+            {
+                for (j = 0; j < bmp.Height - 1; j++)
+                {
+                    VerticalR[i,j] = Math.Abs((rojo[i,j + 1] - rojo[i,j]));
+                    VerticalG[i,j] = Math.Abs((verde[i,j + 1] - verde[i,j]));
+                    VerticalB[i,j] = Math.Abs((azul[i,j + 1] - azul[i,j]));
+                    Nueva_Imagen.SetPixel(i, j, Color.FromArgb(VerticalR[i,j], VerticalG[i,j], VerticalB[i,j]));
+                }
+            }
+        }
+
+        int calculagradiente(int x, int y)
+        {
+            double a, b, c;
+            a = x * x;
+            b = y * y;
+            c = .5 * Math.Sqrt(a + b);
+            return (int)Math.Ceiling(c);
+            
+        }
+
+        void Gradiente() {
+            OperacionVertical();
+            OperacionHorizontal();
+
+            for (i = 0; i < bmp.Width - 1; i++)
+            {
+                for (j = 0; j < bmp.Height; j++)
+                {
+                    r = calculagradiente(HorizontalR[i,j], VerticalR[i,j]);
+                    g = calculagradiente(HorizontalG[i,j], VerticalG[i,j]);
+                    b = calculagradiente(HorizontalB[i,j], VerticalB[i,j]);
+                    //Nueva_Imagen.SetPixel(i, j, Color.FromArgb(VerticalR[i, j], VerticalG[i, j], VerticalB[i, j]));
+                    Nueva_Imagen.SetPixel(i, j, Color.FromArgb(r, g, b));
+                    sum = sum + r + g + b;
+                }
+            }
+
+            bmp = Nueva_Imagen;
+            //mostrar
+            pictureBox1.Image = bmp;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {                
+                Gradiente();
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   //limpiar
+            pictureBox1.Image = bmpBK;
+            bmp = bmpBK;
+        }
+    }
+}
